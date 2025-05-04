39658427e70c3cb8b8146bdaa4f3f28e3fde0c37
SHIMHUN <shimhun88@naver.com>
2025-05-05 02:04:18 +0900

    feat: 계단오르기 PS

---
+++ DP/DP_1/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    static Integer[] DP;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        DP = new Integer[N+1]; //DP배열 초기에 null로 초기화
+
+        DP[1] = 1;
+        DP[2] = 2;
+
+        //1. Down-Top
+        for(int i=3; i<=N; i++){
+            DP[i] = DP[i-1] + DP[i-2];
+        }
+        System.out.println(DP[N]);
+
+//        //2. Top-down
+//        System.out.println(recur(N));
+    }
+
+    static int recur(int N){
+        if(DP[N] == null){
+            DP[N] = recur(N-1) + recur(N-2);
+        }
+
+        return DP[N];
+    }
+}
